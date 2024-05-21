0a5eafc25bbab015a6d7ff13244d46f6ef2a9a12
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-05-21 21:05:31 +0530

    change in hero page

--- Portfolio/src/pages/MainPage.jsx
+++ Portfolio/src/pages/MainPage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import HeaderNav from "./HeaderNav";
+import LocomotiveScroll from 'locomotive-scroll';
+import HeroSection from "./HeroSection";
+import Education from "./Education";
+
+// locmotiveScroll
+const locomotiveScroll = new LocomotiveScroll();
 
 const MainPage = () => {
   return (
     <div>
       <div className=" ">
         <HeaderNav />
+        <HeroSection/>
+        <Education/>
       </div>
     </div>
   );
